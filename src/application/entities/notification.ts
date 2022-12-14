133aa832d1bef689180e9972209bdff59d95edd4
Wesley Alves Rolim <walvesrolim@gmail.com>
2022-12-14 08:59:49 -0300

    feat: create folders structure and notification entity

---
+++ src/application/entities/notification.ts
@@ -0,0 +1,47 @@
+export interface NotificationProps {
+  recipientId: string;
+  content: string;
+  category: string;
+  readAt?: Date | null;
+  createdAt: Date;
+}
+
+export class Notification {
+  constructor(private props: NotificationProps) {}
+
+  get recipientId(): string {
+    return this.props.recipientId;
+  }
+
+  set recipientId(recipientId: string) {
+    this.props.recipientId = recipientId;
+  }
+
+  get content(): string {
+    return this.props.content;
+  }
+
+  set content(content: string) {
+    this.props.content = content;
+  }
+
+  get category(): string {
+    return this.props.category;
+  }
+
+  set category(category: string) {
+    this.props.category = category;
+  }
+
+  get readAt(): Date | null | undefined {
+    return this.props.readAt;
+  }
+
+  set readAt(readAt: Date | null | undefined) {
+    this.props.readAt = readAt;
+  }
+
+  get createdAt(): Date {
+    return this.props.createdAt;
+  }
+}
