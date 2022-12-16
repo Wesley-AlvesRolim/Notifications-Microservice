b39d6af4a225a342c84a3340248e49fcbe0146ea
Wesley Alves Rolim <walvesrolim@gmail.com>
2022-12-16 11:56:44 -0300

    feat: add canceledAt and cancel method to notification

--- src/application/entities/Notification.ts
+++ src/application/entities/Notification.ts
@@ -7,6 +7,7 @@ export interface NotificationProps {
   content: Content;
   category: string;
   readAt?: Date | null;
+  canceledAt?: Date | null;
   createdAt: Date;
 }
 
@@ -48,6 +49,14 @@ export class Notification {
 
   set category(category: string) {
     this.props.category = category;
+  }
+
+  get canceledAt(): Date | null | undefined {
+    return this.props.canceledAt;
+  }
+
+  public cancel() {
+    this.props.canceledAt = new Date();
   }
 
   get readAt(): Date | null | undefined {
