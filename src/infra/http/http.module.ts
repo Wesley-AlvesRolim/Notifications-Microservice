e4f6fe257597174d4b5ce2cc5543840f0cad27ef
Wesley Alves Rolim <walvesrolim@gmail.com>
2022-12-16 18:25:55 -0300

    feat: injects use-cases to NotificationController

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -2,10 +2,22 @@ import { Module } from '@nestjs/common';
 import { SendNotification } from '@application/use-cases/send-notification';
 import { DatabaseModule } from '../database/database.module';
 import { NotificationController } from './controllers/notification.controller';
+import { GetRecipientNotifications } from '@application/use-cases/get-recipient-notifications';
+import { CountRecipientNotification } from '@application/use-cases/count-recipient-notification';
+import { ReadNotification } from '@application/use-cases/read-notification';
+import { UnreadNotification } from '@application/use-cases/unread-notification';
+import { CancelNotification } from '@application/use-cases/cancel-notification';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [NotificationController],
-  providers: [SendNotification],
+  providers: [
+    SendNotification,
+    GetRecipientNotifications,
+    CountRecipientNotification,
+    ReadNotification,
+    UnreadNotification,
+    CancelNotification,
+  ],
 })
 export class HttpModule {}
